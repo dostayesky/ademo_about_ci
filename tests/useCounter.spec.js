de47c37aa89747c28b7791287beeac1b69fdf961
dostayesky <156778419+dostayesky@users.noreply.github.com>
2025-08-07 11:34:12 +0700

    fix useCounter test

--- tests/useCounter.spec.js
+++ tests/useCounter.spec.js
@@ -20,6 +20,8 @@ describe('useCounter', () => {
     const { result } = renderHook(() => useCounter());
     act(() => {
       result.current.setVal(5);
+    });
+    act(() => {
       result.current.increment();
     });
     expect(result.current.count).toBe(5);
@@ -34,8 +36,12 @@ describe('useCounter', () => {
     expect(result.current.count).toBe(2);
     act(() => {
       result.current.setVal(3);
+    });
+
+    act(() => {
       result.current.increment();
     });
+
     expect(result.current.count).toBe(5);
   });
 });
